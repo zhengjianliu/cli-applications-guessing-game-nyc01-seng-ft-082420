e20b0548a527cf2212e279ced5030da3a6d1876a
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-21 16:16:36 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,8 +2,14 @@
 require 'pry'
 def run_guessing_game
   num = rand(6)+1
+  prompt = "Please guess a number between 1 and 6."
   input = gets.chomp
   if num == input
+    puts "You guessed the correct number!"
+  elsif input == "exit"
+    puts "Goodbye!"
+  else
     puts ""
+    
     binding.pry
 end
